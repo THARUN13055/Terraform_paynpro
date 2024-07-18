f8ff6ea4cc5144a383b7fa9873cd23c100afecde
tharun13055 <megatharun13055>
2024-07-18 11:40:53 +0530

    just terraform fmt

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -28,19 +28,19 @@ module "Listener" {
 }
 
 module "Launch_configuration" {
-  source                      = "./modules/Auto_Scaling_Groups/Launch_configuration"
-  launch_configuration_name        = "paynpro-launch-configguration"
-  key_pair_name               = "Mumbai_paynpro"
-  instance_type               = "t2.micro"
-  image_id           = "ami-0ad21ae1d0696ad58"
-  monitoring_enabled = true
-  security_groups             = module.security_group_Webserver.security_groups_ingress_ids
-  ebs_device_name            = "/dev/xvda"
-  volume_type                = "gp3"
-  volume_size                = 8
-  ebs_encrypted              = true
-  delete_on_termination      = true
-  iops                       = 1000
+  source                    = "./modules/Auto_Scaling_Groups/Launch_configuration"
+  launch_configuration_name = "paynpro-launch-configguration"
+  key_pair_name             = "Mumbai_paynpro"
+  instance_type             = "t2.micro"
+  image_id                  = "ami-0ad21ae1d0696ad58"
+  monitoring_enabled        = true
+  security_groups           = module.security_group_Webserver.security_groups_ingress_ids
+  ebs_device_name           = "/dev/xvda"
+  volume_type               = "gp3"
+  volume_size               = 8
+  ebs_encrypted             = true
+  delete_on_termination     = true
+  iops                      = 1000
 }
 
 module "Auto_Scaling_Groups" {
--- modules/Auto_Scaling_Groups/Launch_configuration/Launch_config.tf
+++ modules/Auto_Scaling_Groups/Launch_configuration/Launch_config.tf
@@ -1,20 +1,20 @@
 resource "aws_launch_configuration" "web_launch_configuration" {
-  name                                 = var.launch_configuration_name
-  image_id                             = var.image_id
-  ebs_optimized                        = true
-  instance_type                        = var.instance_type
-  key_name                             = var.key_pair_name
-  security_groups                      = var.security_groups
+  name              = var.launch_configuration_name
+  image_id          = var.image_id
+  ebs_optimized     = true
+  instance_type     = var.instance_type
+  key_name          = var.key_pair_name
+  security_groups   = var.security_groups
   enable_monitoring = var.monitoring_enabled
   ebs_block_device {
-    device_name = var.ebs_device_name
-    volume_type = var.volume_type
-    volume_size = var.volume_size
-    encrypted   = var.ebs_encrypted
+    device_name           = var.ebs_device_name
+    volume_type           = var.volume_type
+    volume_size           = var.volume_size
+    encrypted             = var.ebs_encrypted
     delete_on_termination = var.delete_on_termination
-    iops = var.iops
+    iops                  = var.iops
   }
-    lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
